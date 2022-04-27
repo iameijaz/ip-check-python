a298c08aa4f94d5d2c826a73722570b91dfb5c7d
Ijaz Ahmed <bugtiijaz321@gmail.com>
2022-04-28 04:51:09 +0500

    Add files via upload

---
+++ ip_check.py
@@ -0,0 +1,19 @@
+'''
+CODE FOR CHECKING YOUR IP ADDRESS
+AUTHOR: IJAZ AHMED (VBT)
+'''
+
+# Check my IP Address
+
+import requests 
+
+
+def get_ip():
+    res=requests.get('http://ip.jsontest.com')
+    ip=res.text.split('"')[3].split('"')[0]
+
+
+    #print(f"MY IP ADDRESS IS: {ip}")
+    return ip
+
+
